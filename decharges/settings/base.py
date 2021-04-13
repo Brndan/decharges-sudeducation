446ffa38e56ccebb8f7d3633b6a14fb2da5eaed4
Hugo <hugo@hashbang.fr>
2021-04-13 18:01:42 +0200

    rename app

--- decharges/settings/base.py
+++ decharges/settings/base.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "decharges.decharges",
+    "decharges.timesheet",
 ]
 
 MIDDLEWARE = [
