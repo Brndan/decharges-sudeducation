ceca8c137d5952a6bbea05bc03e753e83f508b3c
Hugo <hugo@hashbang.fr>
2021-04-23 15:22:46 +0200

    parametre: autoriser juste à modifier
    fix https://gitlab.com/hashbangfr/sudeducation/-/issues/34

--- decharges/parametre/admin.py
+++ decharges/parametre/admin.py
@@ -6,5 +6,11 @@ from decharges.parametre.models import ParametresDApplication
 class ParametresDApplicationAdmin(admin.ModelAdmin):
     list_display = ["annee_en_cours", "decharges_editables"]
 
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
 
 admin.site.register(ParametresDApplication, ParametresDApplicationAdmin)
