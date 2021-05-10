1d4910cd7ce1c264ad1722cb92801787ca96c498
Hugo <hugo@hashbang.fr>
2021-05-10 15:15:38 +0200

    syndicats à relancer: syndicats depassant leur quota
    fix https://gitlab.com/hashbangfr/sudeducation/-/issues/33

--- decharges/decharge/views/utils.py
+++ decharges/decharge/views/utils.py
@@ -75,3 +75,56 @@ def aggregation_par_beneficiaire(utilisation_temps_decharges):
         "corps": corps,
         "rnes": rnes,
     }
+
+
+def calcul_repartition_temps(annee_en_cours, federation, syndicat):
+    temps_utilises = syndicat.utilisation_temps_de_decharges_par_annee.filter(
+        annee=annee_en_cours,
+        supprime_a__isnull=True,
+    )
+    temps_utilises_total = sum(
+        temps_consomme.etp_utilises for temps_consomme in temps_utilises
+    )
+    temps_donnes = syndicat.temps_de_decharges_donnes.filter(
+        annee=annee_en_cours,
+    )
+    temps_donnes_total = sum(
+        temps_donne.temps_de_decharge_etp for temps_donne in temps_donnes
+    )
+    temps_decharge_federation = None
+    temps_recus_par_la_federation = 0
+    temps_recus_par_des_syndicats = 0
+    for temps_recu in syndicat.temps_de_decharges_par_annee.filter(
+        annee=annee_en_cours,
+    ):
+        if (
+            temps_recu.syndicat_donateur is not None
+            and temps_recu.syndicat_donateur != federation
+        ):
+            temps_recus_par_des_syndicats += temps_recu.temps_de_decharge_etp
+        else:
+            temps_recus_par_la_federation += temps_recu.temps_de_decharge_etp
+            temps_decharge_federation = temps_recu
+    temps_restant = (
+        temps_recus_par_la_federation
+        + temps_recus_par_des_syndicats
+        - temps_utilises_total
+        - temps_donnes_total
+    )
+    cts_consommes = syndicat.utilisation_cts_ponctuels_par_annee.filter(
+        annee=annee_en_cours
+    ).first()
+    if cts_consommes:
+        temps_restant -= cts_consommes.etp_utilises
+
+    return (
+        cts_consommes,
+        temps_decharge_federation,
+        temps_donnes,
+        temps_donnes_total,
+        temps_recus_par_des_syndicats,
+        temps_recus_par_la_federation,
+        temps_restant,
+        temps_utilises,
+        temps_utilises_total,
+    )
